5cb6f910a9b90cd3e9cefb5a1ad5817357451e90
Henrique Kalife <henriquekalife@gmail.com>
2023-09-23 20:43:54 -0300

    chore: add basic UI

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#D32F0D',
+    },
+    secondary: {
+      main: '#000000',
+    },
+  },
+});
+
+export default theme;
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from '../resources/logo.png';
+import { TextField, Button } from '@mui/material'
 import './Login.css';
 
 function Login() {
@@ -9,10 +10,19 @@ function Login() {
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
             <img
-              className="mx-auto h-10 w-auto"
+              className="mx-auto h-10 w-auto my-12"
               src={logo}
               alt="Pedra Moura"
             />
+          </div>
+          <div className="mb-8">
+            <TextField sx={{ width: '100%' }} label="Usuário" />
+          </div>
+          <div className="mb-8">
+            <TextField sx={{ width: '100%' }} label="Senha" type="password" />
+          </div>
+          <div w-full>
+            <Button variant="contained" fullWidth={true}>Entrar</Button>
           </div>
         </div>
       </div>
